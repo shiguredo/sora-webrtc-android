356ee6c1ea60dbbae83e83d92900a42e7c96141c
Shunichi Shinohara <shino@shiguredo.jp>
2018-05-02 16:14:25 +0900

    jitpack 上でダウンロード & mvn install file するスクリプトを追加する

---
+++ prepareEnvironment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+VERSION=66.8.1
+
+AAR_URL=https://github.com/shiguredo/sora-webrtc-android/releases/download/${VERSION}/libwebrtc.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+curl -L -O ${AAR_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${VERSION} \
+    -DgroupId=com.github.shiguredo \
+    -DartifactId=libwebrtc
